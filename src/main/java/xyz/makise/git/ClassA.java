9cffcbba3219b21347c328517b9ca8090b666d94
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-01-01 22:30:48 +0800

    update: 添加一个新的方法2

--- src/main/java/xyz/makise/git/ClassA.java
+++ src/main/java/xyz/makise/git/ClassA.java
@@ -13,4 +13,8 @@ public class ClassA {
     public void methodB() {
         System.out.println("this is the third version");
     }
+
+    public void methodC() {
+        System.out.println("this is from the fourth version");
+    }
 }
