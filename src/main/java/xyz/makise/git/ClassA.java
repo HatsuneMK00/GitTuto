530145cedb5451a5a1ce47a6015d91a53eaa7dce
makise <10175101106@stu.ecnu.edu.cn>
2020-06-18 17:16:40 +0800

    Revert "update: 添加一个新的方法"
    This reverts commit b993cc68

--- src/main/java/xyz/makise/git/ClassA.java
+++ src/main/java/xyz/makise/git/ClassA.java
@@ -13,11 +13,4 @@ public class ClassA {
     public void methodB() {
         System.out.println("this is the third version");
     }
-
-    public void methodC() {
-        System.out.println("this is from the fourth version");
-    }
-
-    public void methodInjected() {
-    }
 }
